7393f49725bc0381194bfb61404a52de28c3fe8f
Tobias Schmid <tobi@energeer.de>
2019-04-26 13:38:38 +0200

    introduce unity & node routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,37 @@ Rails.application.routes.draw do
   root to: 'status#index'
 
   scope '/v1' do
-    resources :users, only: [:index]
-    # mount_devise_token_auth_for 'User', at: 'auth'
+    # devise authentication routes
     mount_devise_token_auth_for 'User', at: 'auth',
       controllers: {registrations: :registrations, sessions: :sessions}
-
     mount_devise_token_auth_for 'Node', at: 'node_auth',
       controllers: {registrations: :node_registrations, sessions: :node_sessions}
 
+    # user routes
+    # GET /v1/users/
+    resources :users, only: %i[index]
+
+    # unity routes
+    # GET /v1/unities/
+    # POST /v1/unities/
+    # GET /v1/unities/<id>
+    resources :unities, only: %i[show index create] do
+      member do
+        # POST /v1/unities/<id>/node
+        post :nodes
+        # GET /v1/unities/<id>/nodes
+        get :nodes
+      end
+    end
+
+    # node routes
+    # GET /v1/nodes/<id>
+    resources :nodes, only: %i[show] do
+      # POST /v1/nodes/<id>/sensors
+      resources :sensors, only: [:create]
+    end
   end
+
   # for swagger docs
   resources :docs, only: [:index]
 end
